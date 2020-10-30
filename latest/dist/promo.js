0363ff83c8449e0ff95b733dbe9ec5bb5126975d
Weij33t <43062352+Weij33t@users.noreply.github.com>
2020-10-30 13:44:57 +0900

    Another one page

---
+++ latest/dist/promo.js
@@ -0,0 +1,4 @@
+$('.cart-rightside-promo__toggle').on('click', function () {
+  $('.cart-rightside-promo__text').toggleClass('cart-rightside-promo-hidden__text_hide');
+  $('.cart-rightside-promo-hidden').toggleClass('cart-rightside-promo-hidden_show');
+})
